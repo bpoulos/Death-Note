91766568a3596d617a5f0258c444a0bdca03f773
bpoulos <71395409+bpoulos@users.noreply.github.com>
2021-03-23 10:22:56 -0400

    Initial Commit

---
+++ Death Note/Death_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Death_NoteApp.swift
+//  Death Note
+//
+//  Created by Brian on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Death_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
